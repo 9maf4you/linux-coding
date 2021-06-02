7adf33637ffa75ca4889f42ca1d9b06bbee86493
Maxim <ncx2@yandex.ru>
2021-06-02 10:25:56 +0300

    d

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all:
 	gcc -static -g ./stp/3.5/getpid.c -o getpid.bin
+	gcc -static -g ./stp/3.5/fork.c -o fork.bin
 
 
 clean:
---
+++ stp/3.5/fork.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+int forked() {
+    while (1 == 1) {
+        puts("sleeping...");
+        sleep (1);
+    }
+
+}
+
+int main(){
+    
+}
