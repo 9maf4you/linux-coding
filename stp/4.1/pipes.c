97e90800a2d20bdb21df4ad20e430b24f1ba7472
Maxim <ncx2@yandex.ru>
2021-07-28 11:18:03 +0300

    2

--- stp/4.1/pipes.c
+++ stp/4.1/pipes.c
@@ -11,10 +11,8 @@ int zerocount(char zeroes[]) {
     long unsigned int lenarr = strlen(zeroes);
 
     for (counter; counter < strlen(zeroes); ++counter) {
-        //printf("Index Elem %i\n", counter);
-        printf("Value Elem %c\n", zeroes[counter]);
+        //printf("Value Elem %c\n", zeroes[counter]);
         if (zeroes[counter] == zc[0]) {
-            printf("IF\n", zrs);
             ++zrs;
         }
     }
@@ -24,15 +22,12 @@ int zerocount(char zeroes[]) {
 void main(int argc, char* argv[]) {
     printf("argc: %d\n", argc);
     printf("arg[1]: %s\n", argv[1]);
-    printf("arg[1]: %s\n", argv[2]);
+    printf("arg[2]: %s\n", argv[2]);
     if (argc < 2) {
         exit(12);
     }  
+    
+    execve(argv[2], &argv[3], NULL);
     zerocount(argv[2]);
-    execve(argv[1], &argv[2], NULL);
-    //sleep(10);
-    fclose(stdin);
-    //sleep(10);
-    fclose(stdout);
-    //sleep(10);
+    
 }
