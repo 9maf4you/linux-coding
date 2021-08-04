1121415c015ff6229e4edd38dffa2af2f9ed0647
Maxim <ncx2@yandex.ru>
2021-08-04 10:59:36 +0300

    step

--- stp/4.1/pipes.c
+++ stp/4.1/pipes.c
@@ -17,7 +17,7 @@ int zerocount(char zeroes[]) {
             ++zrs;
         }
     }
-    printf("Zero at array: %d\n", zrs);
+    //printf("Zero at array: %d\n", zrs);
     return zrs;
 }
 
@@ -32,16 +32,26 @@ void main(int argc, char* argv[]) {
     if ( fork() == 0 ) {
         //child
         dup2(pipefd[0], STDIN_FILENO);
+        close(4);
+        close(3);
         char cbuf[] = "";
         read(STDIN_FILENO, &cbuf, 16);
-        puts(cbuf);
-        //sleep(300);
+        puts("CHILD START");
+        int zc;
+        zc = zerocount(cbuf);
+        printf("CHILD COUNTED: %d\n", zc);
+        puts("CHILD END");
+        sleep(300);
     } else {
         //parent
-        //char pbuf[] = "STDOUT_FILENO";
+        char pbuf[] = "STDOUT_FILENO";
         dup2(pipefd[1], STDOUT_FILENO);
-        execl(argv[2], argv[2], NULL);
-        //sleep(300); //write(pipefd[1], pbuf, 16);
-
+        close(4);
+        close(3);
+        //The command "./pipes.bin sleep 10"
+        //shows up: execve("/usr/bin/sleep", ["sleep", "10"], 0x7ffff53580c8 /* 18 vars */) =  0
+        //....wierd
+        execlp(argv[1], argv[1], argv[2], NULL);
+        write(pipefd[1], pbuf, 16);
     }
 }
