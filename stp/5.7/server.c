c73f6d2928175c9415800aeb17a697a912197410
Maxim <ncx2@yandex.ru>
2021-09-15 11:16:26 +0300

    step

--- stp/5.7/server.c
+++ stp/5.7/server.c
@@ -1,14 +1,18 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 
 
 void main() {
-    int ss = socket(AF_INET, SOCK_NONBLOCK, SOCK_STREAM);
+    int ss = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server;
-    server.sin_addr.s_addr=INADDR_LOOPBACK;
     server.sin_family = AF_INET;
     server.sin_port = 27015;
+    server.sin_addr.s_addr=INADDR_ANY;
+    
+    
     bind(ss, (struct sockaddr *) &server, sizeof(&server));
+    sleep(300);
 }
