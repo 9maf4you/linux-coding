129a1c1db260d8fedb48a36b3cd9bbe8601caa8b
Maxim <ncx2@yandex.ru>
2021-09-16 11:10:38 +0300

    ya step

--- stp/5.7/server.c
+++ stp/5.7/server.c
@@ -2,17 +2,20 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
-
+#include <strings.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 
 void main() {
     int ss = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server;
     server.sin_family = AF_INET;
-    server.sin_port = 27015;
-    server.sin_addr.s_addr=INADDR_ANY;
+    server.sin_port = htons(27016);
+    server.sin_addr.s_addr = INADDR_ANY;
     
-    
-    bind(ss, (struct sockaddr *) &server, sizeof(&server));
-    sleep(300);
+    bind(ss, (struct sockaddr*)&server, sizeof(server));
+    listen(ss, 1);
+    accept(ss, NULL, NULL);
+    //sleep(300);
 }
