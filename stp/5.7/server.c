559d7c203bcb42233b886cf2316c6002f0d5bda5
Maxim <ncx2@yandex.ru>
2021-09-20 11:50:22 +0300

    Update server.c
    just a simple tcp server which is close connection when gets OFF string

--- stp/5.7/server.c
+++ stp/5.7/server.c
@@ -2,9 +2,10 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <strings.h>
+#include <string.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdio.h>
 
 
 void main() {
@@ -13,11 +14,15 @@ void main() {
     server.sin_family = AF_INET;
     server.sin_port = htons(27016);
     server.sin_addr.s_addr = INADDR_ANY;
-    
+    char buf[24];
+    char OFF[] = "OFF\r\n";
+
     bind(ss, (struct sockaddr*)&server, sizeof(server));
     listen(ss, 1);
-    accept(ss, NULL, NULL);
-    //sleep(300);
-    // Надо собрать трафик и позыркать почему так curl: (56) Recv failure: Connection reset by peer
-    // 
+    int sr = accept(ss, NULL, NULL);
+    while ( strcmp(buf, OFF) ) {
+        recv(sr, &buf, 24, 0);
+        puts(buf);
+    };
+    close(ss);
 }
